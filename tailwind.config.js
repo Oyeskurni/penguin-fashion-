2f55ad69cd54e8324122f0d70d048373377039b7
oyeskurni <mdoyeskuruni72@gmail.com>
2025-01-26 21:48:52 +0530

    all done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: ['Roboto', 'sans-serif'],
+                bebas: ['Bebas Neue', 'cursive'],
+            },
+        },
+    },
+    plugins: [],
+};
